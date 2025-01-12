5b407b20eec1c96d721222715ded1e7584bd88b5
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2025-01-12 02:47:21 +0200

    feat: Add update and delete product routes and service handlers
    - Implement  handler to update product by ID.
    - Implement  handler to delete product by ID.

--- ecommerce-api/routes/productRoute.js
+++ ecommerce-api/routes/productRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import productService from "../services/productService.js";
 
-const { createProduct, getProducts, getProduct } = productService;
+const { createProduct, getProducts, getProduct, updateProduct, deleteProduct } =
+  productService;
 
 const router = express.Router();
 
 router.route("/").post(createProduct).get(getProducts);
 
-router.route("/:id").get(getProduct);
+router.route("/:id").get(getProduct).put(updateProduct).delete(deleteProduct);
 
 export default router;
--- ecommerce-api/services/productService.js
+++ ecommerce-api/services/productService.js
@@ -37,9 +37,44 @@ const getProduct = expressAsyncHandler(async (req, res, next) => {
 
   res.status(200).json({ data: product });
 });
+// @description    update specific product
+// @route          Post  /api/v1/products/:id
+// @access        private
+const updateProduct = expressAsyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await ProductModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+  });
+
+  if (!product) {
+    // return res.status(404).json({ message: 'Category not found' });
+    return next(new ApiError(`No product for this id ${id}`, 404));
+  }
+
+  res.status(200).json({ data: product });
+});
+
+// @description    delete specific product
+// @route          Delete  /api/v1/products/:id
+// @access        private
+const deleteProduct = expressAsyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await ProductModel.findByIdAndDelete(id);
+
+  if (!product) {
+    // return res.status(404).json({ message: 'Category not found' });
+    return next(new ApiError(`No product for this id ${id}`, 404));
+  }
+
+  res.status(204).send();
+});
 
 export default {
   createProduct,
   getProducts,
   getProduct,
+  updateProduct,
+  deleteProduct,
 };
