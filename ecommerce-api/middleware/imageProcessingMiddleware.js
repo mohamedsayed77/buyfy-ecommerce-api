652ec4f8b009ef3e3df13e5a818e39c8fc9ecb9f
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2025-01-15 04:19:10 +0200

    feat: integrate image upload and processing middleware with product
    - Applied multer-based image upload middleware to product routes.
    - Integrated sharp-based image processing for product images.
    - Implemented image processing for both image covers and multiple images.
    - Updated product routes to include the new middleware for creating and updating products.

--- ecommerce-api/middleware/imageProcessingMiddleware.js
+++ ecommerce-api/middleware/imageProcessingMiddleware.js
@@ -37,4 +37,40 @@ const resizeBrandImage = () =>
     next();
   });
 
-export default { resizeCategoryImage, resizeBrandImage };
+const resizeProductImages = AsyncHandler(async (req, res, next) => {
+  // image processing for imageCover
+  if (req.files.imageCover) {
+    const imageCoverFileName = `product-${uuidv4()}-${Date.now()}-cover.jpeg`;
+
+    await sharp(req.files.imageCover[0].buffer)
+      .resize(2000, 1333)
+      .toFormat("jpeg")
+      .jpeg({ quality: 95 })
+      .toFile(`uploads/products/${imageCoverFileName}`);
+
+    // save the imageCover to the database
+    req.body.imageCover = imageCoverFileName;
+  }
+
+  // image processing for images
+  if (req.files.images) {
+    req.body.images = [];
+    await Promise.all(
+      req.files.images.map(async (image, index) => {
+        const imageFileName = `product-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`;
+
+        await sharp(image.buffer)
+          .resize(2000, 1333)
+          .toFormat("jpeg")
+          .jpeg({ quality: 95 })
+          .toFile(`uploads/products/${imageFileName}`);
+
+        // save the image to the database
+        req.body.images.push(imageFileName);
+      })
+    );
+  }
+  next();
+});
+
+export default { resizeCategoryImage, resizeBrandImage, resizeProductImages };
--- ecommerce-api/routes/productRoute.js
+++ ecommerce-api/routes/productRoute.js
@@ -1,6 +1,11 @@
 import express from "express";
 import productService from "../services/productService.js";
 import productValidator from "../utils/validators/productValidator.js";
+import uploadMiddleware from "../middleware/uploadMiddleware.js";
+import resizeImage from "../middleware/imageProcessingMiddleware.js";
+
+const { uploadMixImages } = uploadMiddleware;
+const { resizeProductImages } = resizeImage;
 
 const { createProduct, getProducts, getProduct, updateProduct, deleteProduct } =
   productService;
@@ -11,14 +16,32 @@ const {
   deleteProductValidator,
 } = productValidator;
 
+const uploadProductsImages = uploadMixImages([
+  { name: "imageCover", maxCount: 1 },
+  { name: "images", maxCount: 5 },
+]);
+
 const router = express.Router();
 
-router.route("/").get(getProducts).post(createProductValidator, createProduct);
+router
+  .route("/")
+  .get(getProducts)
+  .post(
+    uploadProductsImages,
+    resizeProductImages,
+    createProductValidator,
+    createProduct
+  );
 
 router
   .route("/:id")
   .get(getProductValidator, getProduct)
-  .put(updateProductValidator, updateProduct)
+  .put(
+    uploadProductsImages,
+    resizeProductImages,
+    updateProductValidator,
+    updateProduct
+  )
   .delete(deleteProductValidator, deleteProduct);
 
 export default router;
